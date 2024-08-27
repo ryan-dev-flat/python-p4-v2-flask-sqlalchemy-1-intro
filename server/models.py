4527538ffda3660cd89189ea5a55c4110db63825
Ryan <coap.ryanmontgomery@gmail.com>
2024-08-26 19:15:40 -0600

    redone

--- server/models.py
+++ server/models.py
@@ -7,6 +7,8 @@ metadata = MetaData()
 # create the Flask SQLAlchemy extension
 db = SQLAlchemy(metadata=metadata)
 
+
+
 # define a model class by inheriting from db.Model.
 
 
@@ -16,3 +18,11 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True)
+    verified = db.Column(db.Boolean, default=False)
